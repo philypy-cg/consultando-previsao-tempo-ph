822d798b2fa77befdc2ac42c874bea610b71564d
philypy-cg <119917190+philypy-cg@users.noreply.github.com>
2023-01-20 00:36:31 -0300

    quarto dia commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+// Variáveis e seleção de elementos
 const apikey = "073ab7377c6433a6b17bf7541ea2857d";
 const apiCountryURL = "https://countryflagsapi.com/png/";
 
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+// Funções
+
+
+
+// Eventos
+searchBtn.addEventListener("click", () => {
+
+    e.preventDefaul();
+
+    const city = cityInput.value;
+
+    console.log("city");
+
+})
